0c815a07617f817b93757bcdcf713627520fd4cb
CeliaHan <hanxiao719@gmail.com>
2022-05-24 20:12:10 +0800

    4 files modified

--- src/app/toolbar-overview-example.ts
+++ src/app/toolbar-overview-example.ts
@@ -9,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['toolbar-overview-example.css'],
 })
 export class ToolbarOverviewExample {
-  id: string = '000';
+  input: string = '';
+  user: People = { id: '', name: '', number: '000' };
+
+  getUser() {
+    this.user = USER_LIST.filter((item) => item.id === this.input)[0];
+  }
+
+  print() {
+    window.print();
+    // window.close();
+  }
+}
+
+const USER_LIST = [
+  { id: '124444', name: '111', number: '011' },
+  { id: '224444', name: '222', number: '222' },
+];
+
+interface People {
+  id: string;
+  name: string;
+  number: string;
 }
